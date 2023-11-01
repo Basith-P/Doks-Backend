5d2c3cfdf7ccd0190dfe14478ed9cc1b0b1999a2
Basith P <basithpofficial@gmail.com>
2023-11-01 21:08:16 +0530

    feat: update document

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -6,7 +6,7 @@ const documentRouter = express.Router();
 
 documentRouter.get("/", auth, async (req, res) => {
   try {
-    const documents = await Document.find({ owner: req.user });
+    const documents = await Document.find({ ownerId: req.user });
     res.json(documents);
   } catch (err) {
     console.log(err);
@@ -20,6 +20,19 @@ documentRouter.post("/", auth, async (req, res) => {
   try {
     const document = Document({ title, content, createdAt, ownerId: req.user });
     await document.save();
+    res.json(document);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+documentRouter.put("/:id/title", auth, async (req, res) => {
+  const id = req.params.id;
+  const { title } = req.body;
+
+  try {
+    const document = await Document.findByIdAndUpdate(id, { title }, { new: true });
     res.json(document);
   } catch (err) {
     console.log(err);
